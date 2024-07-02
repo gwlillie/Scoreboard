cb0b033ac49fc4ca27c1b8c7b3648a7a3591427a
gwlillie <gwlillie12@gmail.com>
2024-07-02 10:22:02 -0400

    Removed unnecessary files

--- pyautoguiMouse.py
+++
@@ -1,8 +0,0 @@
-import pyautogui
-from time import sleep
-
-sleep(3)
-#print(pyautogui.position())
-
-#pyautogui.click(x=280, y=10, clicks=1, interval=1, button='left')
-pyautogui.click(x=144, y=14, clicks=1, interval=1, button='left')
--- run2.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-import time
-
-cmd = 'obs';
-p = subprocess.Popen('exec ' + cmd, stdout=subprocess.PIPE, shell=True)
-time.sleep(15)
-p.kill()
